f9b02202f5ad9d1f061421bc35d74cb205e7f3ac
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-02-23 09:00:03 +0100

    use  instead of sample in demo

--- sample-data/run_anatomy_tutorial.sh
+++ sample-data/run_anatomy_tutorial.sh
@@ -6,20 +6,19 @@ if [ $# -ne 1 ] ; then
 fi
 
 MNE_sample=$1
+export SUBJECT=sample
 
 cd ${MNE_sample}/subjects && export SUBJECTS_DIR=`pwd`
 cd -
-cd ${MNE_sample}/MEG/sample && export MEG_DIR=`pwd`
+cd ${MNE_sample}/MEG/${SUBJECT} && export MEG_DIR=`pwd`
 cd -
-
-export SUBJECT=sample
 
 # Compute BEM model using Flash sequences
 # assumes mef05.mgz and mef30.mgz are in the mri/flash folder
 mne_flash_bem --noconvert
 
 # The BEM surfaces should now be the bem/flash folder
-cd ${SUBJECTS_DIR}/sample/bem
+cd ${SUBJECTS_DIR}/${SUBJECT}/bem
 ln -s flash/inner_skull.surf .
 ln -s flash/outer_skin.surf .
 ln -s flash/outer_skull.surf .
@@ -28,8 +27,8 @@ ln -s flash/outer_skull.surf .
 # mne_setup_mri --overwrite
 
 # Make high resolution head surface
-mkheadsurf -s sample
-mne_surf2bem --surf ${SUBJECTS_DIR}/sample/surf/lh.seghead --id 4 --check --fif ${SUBJECTS_DIR}/sample/bem/sample-head.fif
+mkheadsurf -s ${SUBJECT}
+mne_surf2bem --surf ${SUBJECTS_DIR}/${SUBJECT}/surf/lh.seghead --id 4 --check --fif ${SUBJECTS_DIR}/${SUBJECT}/bem/${SUBJECT}-head.fif
 
 # Generate morph maps for morphing between sample and fsaverage
-mne_make_morph_maps --from sample --to fsaverage
+mne_make_morph_maps --from ${SUBJECT} --to fsaverage
