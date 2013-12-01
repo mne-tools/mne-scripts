c56b519d07d99f7fb4e3a9840550f9767c07442c
Denis A. Engemann <d.engemann@fz-juelich.de>
2013-12-01 15:49:05 +0100

    ENH: more ln + style improvements
    ...

--- sample-data/run_anatomy_tutorial.sh
+++ sample-data/run_anatomy_tutorial.sh
@@ -19,27 +19,22 @@ mne_flash_bem --noconvert
 
 # The BEM surfaces should now be the bem/flash folder
 cd ${SUBJECTS_DIR}/${SUBJECT}/bem
-ln -s flash/inner_skull.surf .
-ln -s flash/outer_skin.surf .
-ln -s flash/outer_skull.surf .
+ln -sf flash/inner_skull.surf .
+ln -sf flash/outer_skin.surf .
+ln -sf flash/outer_skull.surf .
 
 # without FLASH images you should use
 # mne_watershed_bem
-# ln -s watershed/${SUBJECT}_inner_skull_surface ${SUBJECT}-inner_skull.surf
-# ln -s watershed/${SUBJECT}_outer_skin_surface ${SUBJECT}-outer_skin.surf
-# ln -s watershed/${SUBJECT}_outer_skull_surface ${SUBJECT}-outer_skull.surf
-
-# Make high resolution head surface
-head=${SUBJECTS_DIR}/${SUBJECT}/bem/${SUBJECT}-head.fif
-if [ -e $head ]; then
-	echo deleting existing head surface ${head}
-	rm -f $head
-fi
+# ln -sf watershed/${SUBJECT}_inner_skull_surface ${SUBJECT}-inner_skull.surf
+# ln -sf watershed/${SUBJECT}_outer_skin_surface ${SUBJECT}-outer_skin.surf
+# ln -sf watershed/${SUBJECT}_outer_skull_surface ${SUBJECT}-outer_skull.surf
 
 mne make_scalp_surfaces -s ${SUBJECT} -o  # on failure use --force option.
-echo linking ${head_medium} as main head surface
+
 head_medium=${SUBJECTS_DIR}/${SUBJECT}/bem/${SUBJECT}-head-medium.fif
-ln -s $head_medium $head
+head=${SUBJECTS_DIR}/${SUBJECT}/bem/${SUBJECT}-head.fif
+echo linking ${head_medium} as main head surface
+ln -sf $head_medium $head
 
 # Generate morph maps for morphing between sample and fsaverage
 mne_make_morph_maps --from ${SUBJECT} --to fsaverage
