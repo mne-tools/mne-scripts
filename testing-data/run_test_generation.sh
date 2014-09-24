6b77bea611c2e8eb98f040f22c386125ab238609
Eric Larson <larson.eric.d@gmail.com>
2014-09-24 16:41:08 -0700

    WIP: More lines

--- testing-data/run_test_generation.sh
+++ testing-data/run_test_generation.sh
@@ -11,11 +11,14 @@ mne_setup_forward_model --subject sample --surf --ico 3
 # SRC and Fwd
 mne_setup_source_space --oct 4 --overwrite
 mne_add_patch_info --src sample-oct-4-src.fif --srcp sample-oct-4-src.fif
+mne_setup_source_space --oct 2 --overwrite
 NAME=sample_audvis_trunc
 FWD=${NAME}-meg-eeg-oct-6-fwd.fif
 FWD_SMALL=${NAME}-meg-eeg-oct-4-fwd.fif
+FWD_TINY_GRAD=${NAME}-meg-eeg-oct-2-grad-fwd.fif
 mne_do_forward_solution --mindist 5 --spacing oct-6 --meas ${NAME}_raw.fif --bem sample-1280-1280-1280 --overwrite --fwd ${FWD}
 mne_do_forward_solution --mindist 5 --spacing oct-4 --meas ${NAME}_raw.fif --bem sample-1280-1280-1280 --overwrite --fwd ${FWD_SMALL}
+mne_do_forward_solution --mindist 5 --spacing oct-2 --meas ${NAME}_raw.fif --bem sample-1280-1280-1280 --overwrite --grad --fwd ${FWD_TINY_GRAD}
 
 # Ave/cov
 mne_process_raw --raw ../../../MNE-sample-data/MEG/sample/sample_audvis_raw.fif --lowpass 80 --save sample_audvis_trunc_raw.fif --projoff --decim 2
@@ -66,3 +69,29 @@ mne_sensitivity_map --fwd sample_audvis_trunc-meg-vol-7-fwd.fif \
     --map 1 --w sample_audvis_trunc-grad-vol-7-fwd-sensmap
 
 mne_do_inverse_operator --fwd sample_audvis_trunc-meg-vol-7-fwd.fif --depth --meg
+exit(0)
+
+echo "
+# This was used to create the lower-res sample_ds surfaces
+from os import path as op
+import numpy as np
+import mne
+
+subjects_dir = ('/home/larsoner/custombuilds/mne-python/examples/'
+                'MNE-testing-data/subjects')
+for subj in ('sample', 'fsaverage'):
+    src = mne.setup_source_space(subj, None, 'ico5', add_dist=False,
+                                 subjects_dir=subjects_dir)
+    for s, hemi in zip(src, ['lh', 'rh']):
+        print('Subject %s hemi %s' % (subj, hemi))
+        rr = mne.read_surface(op.join(subjects_dir, subj, 'surf',
+                                      hemi + '.sphere.reg'))[0]
+        assert len(rr) == len(s['rr'])
+        rr = rr[s['vertno']]
+        tris = s['use_tris']
+        rev_idx = len(s['rr']) * np.ones(len(s['rr']), int)
+        rev_idx[s['vertno']] = np.arange(len(rr))
+        tris = rev_idx[tris]
+        mne.write_surface(op.join(subjects_dir, subj + '_ds', 'surf',
+                                  hemi + '.sphere.reg'), rr, tris)
+"
